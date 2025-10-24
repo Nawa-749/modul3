d654c9e5b4623fe2433b90bbbaa2c1b5e79c7f95
Nawa-749 <nawaistiqomah@gmail.com>
2025-10-24 14:01:09 +0700

    yang biasa aja

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String kalimat1 = "Haloo! Mas, kenalan yuk!";
+        String kalimat2 = "08- berapa mas?";
+        System.out.println("Berikut: " + kalimat1 + " " + kalimat2);
+    }
+}
